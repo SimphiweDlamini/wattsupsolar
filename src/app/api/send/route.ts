0fef3c8c63ee640a1c2ce0f230c457ecbc6118b9
SimphiweDlamini <Simphiwed68@gmail.com>
2026-02-26 16:31:10 +0200

    Fix Resend API KEY

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend('re_eksskEwr_FJSmX6bcBZVazwrnJ87yknqW'); 
+const resend = new Resend(process.env.RESEND_API_KEY); 
 
 export async function POST(req: Request) {
   try {
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Text>My Ass</Text>
+    </>
+  );
+};
+
+export default page;
